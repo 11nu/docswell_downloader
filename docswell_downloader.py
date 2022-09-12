1dd7947ceabdf6866e42830370b08ca74e73d34b
11nu <90890110+11nu@users.noreply.github.com>
2022-09-13 03:44:12 +0900

    Fix flake8 errors

--- docswell_downloader.py
+++ docswell_downloader.py
@@ -25,8 +25,10 @@ from urllib.parse import urlparse
 # Override here the URL of the docswell slide you want to download.
 slide_url = "https://www.docswell.com/s/ku-suke/LK7J5V-hello-docswell"
 
+
 def download_image(url, headers={}, timeout=5):
-    response = requests.get(url, allow_redirects=False, timeout=timeout, headers=headers)
+    response = requests.get(
+        url, allow_redirects=False, timeout=timeout, headers=headers)
 
     if response.status_code != 200:
         err = Exception(f"HTTP status: {response.status_code}")
@@ -39,27 +41,30 @@ def download_image(url, headers={}, timeout=5):
 
     return response.content
 
+
 def main():
-    SLIDE_FILENAME = urlparse(slide_url).path.split("/")[3] # LK7J5V-hello-docswell
-    SLIDE_KEY = SLIDE_FILENAME[:6] # LK7J5V
+    SLIDE_FILENAME = urlparse(slide_url).path.split("/")[3]  # LK7J5V-hello-docswell
+    SLIDE_KEY = SLIDE_FILENAME[:6]  # LK7J5V
     EMBED_URL = f"https://www.docswell.com/slide/{SLIDE_KEY}/embed"
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     OUTPUT_DIR = os.path.join(BASE_DIR, "output")
 
-    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"  # noqa: E501
     request_headers = {
         "User-Agent": USER_AGENT,
     }
 
-    html = requests.get(EMBED_URL,headers=request_headers)
+    html = requests.get(EMBED_URL, headers=request_headers)
     soup = BeautifulSoup(html.content, "html.parser")
-    thumbnail_image_elements = soup.find("main", id="player").find_all("li", class_="splide__slide c-thumbnail")
+    thumbnail_image_elements = soup.find(
+        "main", id="player").find_all("li", class_="splide__slide c-thumbnail")
 
     slide_image_urls = []
     for element in thumbnail_image_elements:
-        thumbnail_image_url = element.find("img")["data-splide-lazy"]
-        slide_image_url = urlparse(thumbnail_image_url)._replace(query="").geturl().replace("thumbnail", "page")
+        thumbnail_image_url = urlparse(
+            element.find("img")["data-splide-lazy"])._replace(query="").geturl()
+        slide_image_url = thumbnail_image_url.replace("thumbnail", "page")
 
         slide_image_urls.append(slide_image_url)
 
@@ -69,11 +74,13 @@ def main():
 
         slide_images.append(slide_image)
 
-    abs_output_filepath = os.path.join(OUTPUT_DIR, os.path.basename(f"{SLIDE_FILENAME}.pdf"))
+    abs_output_filepath = os.path.join(
+        OUTPUT_DIR, os.path.basename(f"{SLIDE_FILENAME}.pdf"))
     with open(abs_output_filepath, "wb") as pdf_file:
         pdf_file.write(img2pdf.convert(slide_images))
 
     print(f"The file has been saved: {abs_output_filepath}")
+
 
 if __name__ == "__main__":
     main()
